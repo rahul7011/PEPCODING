bb72077a9ca2063143f5c118b6ad949938742b68
rahul7011 <champrahul01@gmail.com>
2021-09-07 18:05:46 +0530

    lecture 4 Homework better approach

--- level1/BasicsOfProgramming/GettingStarted/RotateANumber.java
+++ level1/BasicsOfProgramming/GettingStarted/RotateANumber.java
@@ -5,25 +5,34 @@ public class RotateANumber {
         Scanner scn = new Scanner(System.in);
         int num = scn.nextInt();
         int k = scn.nextInt();
-        int place = 0;
+        // first the number of digits
+        int nod = 0; // no of digits
         int temp = num;
-        int digitCount = 0;
         while (temp != 0) {
             temp /= 10;
-            digitCount++;
+            nod++;
         }
-        k = k % digitCount;
-        int rotate = 0;
-        while (num != 0) {
-            int q = num / 10;
-            int r = num % 10;
-            int newpos = place + (digitCount - k);
-            newpos = newpos % digitCount;
-            num = q;
-            place++;
-            rotate = rotate + r * (int) Math.pow(10, newpos);
+        // To remove the redundancy of same rotation
+        k = k % nod;
+        // To change the left(-ve) rotation to right rotation(+ve)
+        if (k < 0) {
+            k = k + nod;
         }
-        System.out.println(rotate);
+        // now forming divisor and multiplier;
+        int div = 1;
+        int mul = 1;
+        for (int i = 1; i <= nod; i++) {
+            if (i <= k) {
+                div *= 10;
+            } else {
+                mul *= 10;
+            }
+        }
+        // forming the answer using divisor and multiplier
+        int q = num / div;
+        int r = num % div;
+        int ans = r * mul + q;
+        System.out.println(ans);
         scn.close();
     }
 }
