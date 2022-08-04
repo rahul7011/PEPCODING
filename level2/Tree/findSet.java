e40eb72d20716f57df5c322506b86b2a43dca866
rahul7011 <champrahul01@gmail.com>
2022-08-04 10:56:08 +0530

    findSet

--- level2/Tree/findSet.java
+++ level2/Tree/findSet.java
@@ -240,29 +240,30 @@ public class findSet {
 
     // Burning Trees with description(which node will burn at which time)
     // https://www.geeksforgeeks.org/burn-the-binary-tree-starting-from-the-target-node/
-    private static void burnTrees(TreeNode root,TreeNode blocked,int k,HashMap<Integer,HashSet<Integer>>hm)
-    {
-        if(root==null||root==blocked)
-        {
-            return ;
+    private static void burnTrees(TreeNode root, TreeNode blocked, int k, HashMap<Integer, HashSet<Integer>> hm) {
+        if (root == null || root == blocked) {
+            return;
         }
-        HashSet<Integer>hs=hm.getOrDefault(k, new HashSet<>());
+        HashSet<Integer> hs = hm.getOrDefault(k, new HashSet<>());
         hs.add(root.val);
         hm.put(k, hs);
-        burnTrees(root.left,blocked,k+1,hm);
-        burnTrees(root.right,blocked,k+1,hm);
+        burnTrees(root.left, blocked, k + 1, hm);
+        burnTrees(root.right, blocked, k + 1, hm);
     }
+
     private static void burnTrees() {
-        /*  12
-           /  \
-          13  10
-              / \
-             14 15
-            / \ / \
-          21 24 22 23
- 
-        Let us create Binary Tree as shown
-        above */
+        /*
+         * 12
+         * / \
+         * 13 10
+         * / \
+         * 14 15
+         * / \ / \
+         * 21 24 22 23
+         * 
+         * Let us create Binary Tree as shown
+         * above
+         */
         TreeNode root = new TreeNode(12);
         root.left = new TreeNode(13);
         root.right = new TreeNode(10);
@@ -277,35 +278,34 @@ public class findSet {
         ArrayList<TreeNode> ans = new ArrayList<>();
         int data = 14;
         nodeToRootPath(root, data, ans);
-        HashMap<Integer,HashSet<Integer>>hm=new HashMap<>();
-        TreeNode blocked=null;
-        for(int i=0;i<ans.size();i++)
-        {
-            TreeNode node=ans.get(i);
-            burnTrees(node,blocked,i,hm);
-            blocked=node;
+        HashMap<Integer, HashSet<Integer>> hm = new HashMap<>();
+        TreeNode blocked = null;
+        for (int i = 0; i < ans.size(); i++) {
+            TreeNode node = ans.get(i);
+            burnTrees(node, blocked, i, hm);
+            blocked = node;
         }
-        for(int key:hm.keySet())
-        {
-            System.out.println(key+"-> "+hm.get(key));
+        for (int key : hm.keySet()) {
+            System.out.println(key + "-> " + hm.get(key));
         }
     }
 
     // https://practice.geeksforgeeks.org/problems/burning-tree/1
-    class Solution2
-    {
-        /*class Node {
-            int data;
-            Node left;
-            Node right;
-        
-            Node(int data) {
-                this.data = data;
-                left = null;
-                right = null;
-            }
-        }*/
-       public static boolean nodeToRootPath(Node root, int data, ArrayList<Node> ans) {
+    class Solution2 {
+        /*
+         * class Node {
+         * int data;
+         * Node left;
+         * Node right;
+         * 
+         * Node(int data) {
+         * this.data = data;
+         * left = null;
+         * right = null;
+         * }
+         * }
+         */
+        public static boolean nodeToRootPath(Node root, int data, ArrayList<Node> ans) {
             if (root == null) {
                 return false;
             }
@@ -319,78 +319,249 @@ public class findSet {
             }
             return res;
         }
-    
-        private static void burnTrees(Node root,Node blocked,int k,HashMap<Integer,HashSet<Integer>>hm)
-        {
-            if(root==null||root==blocked)
-            {
-                return ;
+
+        private static void burnTrees(Node root, Node blocked, int k, HashMap<Integer, HashSet<Integer>> hm) {
+            if (root == null || root == blocked) {
+                return;
             }
-            HashSet<Integer>hs=hm.getOrDefault(k, new HashSet<>());
+            HashSet<Integer> hs = hm.getOrDefault(k, new HashSet<>());
             hs.add(root.data);
             hm.put(k, hs);
-            burnTrees(root.left,blocked,k+1,hm);
-            burnTrees(root.right,blocked,k+1,hm);
+            burnTrees(root.left, blocked, k + 1, hm);
+            burnTrees(root.right, blocked, k + 1, hm);
         }
-        public static int minTime(Node root, int data) 
-        {
+
+        public static int minTime(Node root, int data) {
             ArrayList<Node> ans = new ArrayList<>();
             nodeToRootPath(root, data, ans);
-            HashMap<Integer,HashSet<Integer>>hm=new HashMap<>();
-            Node blocked=null;
-            for(int i=0;i<ans.size();i++)
-            {
-                Node node=ans.get(i);
-                burnTrees(node,blocked,i,hm);
-                blocked=node;
+            HashMap<Integer, HashSet<Integer>> hm = new HashMap<>();
+            Node blocked = null;
+            for (int i = 0; i < ans.size(); i++) {
+                Node node = ans.get(i);
+                burnTrees(node, blocked, i, hm);
+                blocked = node;
             }
             // for(int key:hm.keySet())
             // {
-            //     System.out.println(key+"-> "+hm.get(key));
+            // System.out.println(key+"-> "+hm.get(key));
             // }
-            return hm.size()-1;
+            return hm.size() - 1;
         }
     }
-    //Burning Tree optimised
-    public static void burningTreeNode(TreeNode root, int time, TreeNode blockNode, ArrayList<ArrayList<Integer>> ans) {
-        if (root == null || root == blockNode)
-            return;
-        if (time == ans.size())    // if(time == ans.size()) ans.push_back({});
-            ans.add(new ArrayList<>());
-        ans.get(time).add(root.val);
 
-        burningTreeNode(root.left, time + 1, blockNode, ans);
-        burningTreeNode(root.right, time + 1, blockNode, ans);
+    // Burning Tree optimised
+    class Solution3 {
+        /*
+         * class Node {
+         * int data;
+         * Node left;
+         * Node right;
+         * 
+         * Node(int data) {
+         * this.data = data;
+         * left = null;
+         * right = null;
+         * }
+         * }
+         */
+        private static void burnTreeNode(Node root, Node blocked, int time, ArrayList<ArrayList<Integer>> ans) {
+            if (root == null || root == blocked) {
+                return;
+            }
+            if (time == ans.size()) {
+                ans.add(new ArrayList<>());
+            }
+            ans.get(time).add(root.data);
+            burnTreeNode(root.left, blocked, time + 1, ans);
+            burnTreeNode(root.right, blocked, time + 1, ans);
+        }
 
+        public static int burningTree(Node root, int target, ArrayList<ArrayList<Integer>> ans) {
+            if (root == null) {
+                return -1;
+            }
+            if (root.data == target) {
+                burnTreeNode(root, null, 0, ans);
+                return 1;
+            }
+            int lt = burningTree(root.left, target, ans);
+            if (lt != -1) {
+                burnTreeNode(root, root.left, lt, ans);
+                return lt + 1;
+            }
+            int rt = burningTree(root.right, target, ans);
+            if (rt != -1) {
+                burnTreeNode(root, root.right, rt, ans);
+                return rt + 1;
+            }
+            return -1;
+        }
+
+        public static void burningTree(Node root, int data) {
+            ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+            burningTree(root, data, ans);
+        }
+
+        public static int minTime(Node root, int target) {
+            ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+            burningTree(root, target, ans);
+            return ans.size() - 1;
+        }
     }
 
-    public static int burningTree(TreeNode root, int fireNode, ArrayList<ArrayList<Integer>> ans) {
-        if (root == null)
+    // Burning Tree Variance where some nodes contains water
+    private static void burnTreeNodeWithWater(Node root, Node blocked, int time, HashSet<Integer> waterSet,
+            ArrayList<ArrayList<Integer>> ans) {
+        if (root == null || root == blocked || waterSet.contains(root.data) == true) {
+            return;
+        }
+        if (time == ans.size()) {
+            ans.add(new ArrayList<>());
+        }
+        ans.get(time).add(root.data);
+        burnTreeNodeWithWater(root.left, blocked, time + 1, waterSet, ans);
+        burnTreeNodeWithWater(root.right, blocked, time + 1, waterSet, ans);
+    }
+
+    public static int burningTreeWithWater(Node root, int target, HashSet<Integer> waterSet,
+            ArrayList<ArrayList<Integer>> ans) {
+        if (root == null) {
             return -1;
-        if (root.val == fireNode) {
-            burningTreeNode(root, 0, null, ans);
+        }
+        if (root.data == target) {
+            burnTreeNodeWithWater(root, null, 0, waterSet, ans);
             return 1;
         }
-
-        int lt = burningTree(root.left, fireNode, ans);
+        int lt = burningTreeWithWater(root.left, target, waterSet, ans);
         if (lt != -1) {
-            burningTreeNode(root, lt, root.left, ans);
+            if (waterSet.contains(root.data) == true) {
+                // edge case:when a node that is in the nodeToRootPath contains water then we
+                // need to discard all the nodeToRootPath nodes from that point
+                return -1;
+            }
+            burnTreeNodeWithWater(root, root.left, lt, waterSet, ans);
             return lt + 1;
         }
-
-        int rt = burningTree(root.right, fireNode, ans);
+        int rt = burningTreeWithWater(root.right, target, waterSet, ans);
         if (rt != -1) {
-            burningTreeNode(root, lt, root.right, ans);
+            if (waterSet.contains(root.data) == true) {
+                // edge case:when a node that is in the nodeToRootPath contains water then we
+                // need to discard all the nodeToRootPath nodes from that point
+                return -1;
+            }
+            burnTreeNodeWithWater(root, root.right, rt, waterSet, ans);
             return rt + 1;
         }
-
         return -1;
     }
-    public static void burningTree(TreeNode root, int data) {
+
+    public static void burningTreeWithWater(Node root, int data, HashSet<Integer> waterSet) {
         ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
-        burningTree(root, data, ans);
+        burningTreeWithWater(root, data, waterSet, ans);
     }
+
+    public static int minTime() {
+        Node root = new Node(12);
+        root.left = new Node(13);
+        root.right = new Node(10);
+        root.right.left = new Node(14);
+        root.right.right = new Node(15);
+        Node left = root.right.left;
+        Node right = root.right.right;
+        left.left = new Node(21);
+        left.right = new Node(24);
+        right.left = new Node(22);
+        right.right = new Node(23);
+        int target = 22;
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        HashSet<Integer> waterSet = new HashSet<>();
+        waterSet.add(10);
+        waterSet.add(14);
+        if (waterSet.contains(target) == true) {
+            // this means we can't burn the target node as it contains water
+            return 0;
+        }
+        burningTreeWithWater(root, target, waterSet, ans);
+        for (ArrayList<Integer> l : ans) {
+            System.out.println(l);
+        }
+        return ans.size() - 1;
+    }
+
+    // 236. Lowest Common Ancestor of a Binary Tree
+    class Solution4 {
+        private static ArrayList<TreeNode> nodeToRootPath(TreeNode root, TreeNode data) {
+            if (root == null) {
+                return new ArrayList<>();
+            }
+            if (root == data) {
+                ArrayList<TreeNode> temp = new ArrayList<>();
+                temp.add(root);
+                return temp;
+            }
+            ArrayList<TreeNode> left = nodeToRootPath(root.left, data);
+            if (left.size() > 0) {
+                left.add(root);
+                return left;
+            }
+            ArrayList<TreeNode> right = nodeToRootPath(root.right, data);
+            if (right.size() > 0) {
+                right.add(root);
+                return right;
+            }
+            return new ArrayList<>();
+        }
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            ArrayList<TreeNode> p1 = nodeToRootPath(root, p);
+            ArrayList<TreeNode> p2 = nodeToRootPath(root, q);
+            TreeNode LCA = null;
+            int i = p1.size() - 1, j = p2.size() - 1;
+            while (Math.min(i, j) >= 0) {
+                if (p1.get(i) != p2.get(j)) {
+                    return LCA;
+                }
+                LCA = p1.get(i);
+                i--;
+                j--;
+            }
+            return LCA;
+        }
+    }
+
+    // 236. Lowest Common Ancestor of a Binary Tree
+    //Optimised:O(1) space
+    class Solution5 {
+        public TreeNode LCA = null;
+
+        private boolean lca(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) {
+                return false;
+            }
+            boolean selfPresent = (root == p) || (root == q);
+            boolean leftPresent = lca(root.left, p, q);
+            if (LCA != null) {
+                return true;
+            }
+            boolean rightPresent = lca(root.right, p, q);
+            if (LCA != null) {
+                return true;
+            }
+            if ((leftPresent && rightPresent) || (leftPresent && selfPresent) || (rightPresent && selfPresent)) {
+                LCA = root;
+            }
+            return leftPresent || rightPresent || selfPresent;
+
+        }
+
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            lca(root, p, q);
+            return LCA;
+        }
+    }
+
     public static void main(String[] args) {
-        burnTrees();
+        // burnTrees();
+        // minTime();
     }
 }
