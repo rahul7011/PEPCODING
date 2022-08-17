5223e60b02623fea2804a9bc48c9907f7df185c6
rahul7011 <champrahul01@gmail.com>
2022-08-17 23:56:48 +0530

    cuttype

--- level2/DP/cutType.java
+++ level2/DP/cutType.java
@@ -100,6 +100,8 @@ public class cutType {
         }
     }
 
+    // Minimum and Maximum values of an expression with * and +
+    // https://www.geeksforgeeks.org/minimum-maximum-values-expression/
     public static class Pair {
         int max;
         int min;
@@ -163,12 +165,94 @@ public class cutType {
         return dp[si][ei] = currCost;
     }
 
+    // Boolean Parenthesization
+    // https://practice.geeksforgeeks.org/problems/boolean-parenthesization5610/1
+    class Solution2 {
+        public static class Pair {
+            int TotalTrue = 0;
+            int TotalFalse = 0;
+
+            Pair(int TotalTrue, int TotalFalse) {
+                this.TotalTrue = TotalTrue;
+                this.TotalFalse = TotalFalse;
+            }
+        }
+
+        private static void evaluateTrue(Pair left, Pair right, Pair res, char symbol) {
+            int mod = 1003;
+            int totalTrueFalse = ((left.TotalTrue + left.TotalFalse) % mod
+                    * (right.TotalTrue + right.TotalFalse) % mod) % mod;
+            int TotalFalse = 0, TotalTrue = 0;
+            if (symbol == '|') {
+                TotalFalse = (left.TotalFalse * right.TotalFalse) % mod;
+                TotalTrue = (totalTrueFalse - TotalFalse + mod) % mod;
+            } else if (symbol == '&') {
+                TotalTrue = (left.TotalTrue * right.TotalTrue) % mod;
+                TotalFalse = (totalTrueFalse - TotalTrue + mod) % mod;
+            } else if (symbol == '^') {
+                TotalTrue = ((left.TotalTrue * right.TotalFalse) % mod + (left.TotalFalse * right.TotalTrue) % mod)
+                        % mod;
+                TotalFalse = (totalTrueFalse - TotalTrue + mod) % mod;
+            }
+            res.TotalTrue = (res.TotalTrue + TotalTrue) % mod;
+            res.TotalFalse = (res.TotalFalse + TotalFalse) % mod;
+        }
+
+        private static Pair countWays(String s, int si, int ei, Pair[][] dp) {
+            if (si == ei) {
+                int t = s.charAt(si) == 'T' ? 1 : 0;
+                int f = s.charAt(si) == 'F' ? 1 : 0;
+                dp[si][ei] = new Pair(t, f);
+                return dp[si][ei];
+            }
+            if (dp[si][ei] != null) {
+                return dp[si][ei];
+            }
+            Pair res = new Pair(0, 0);
+            for (int cut = si + 1; cut < ei; cut += 2) {
+                Pair leftPair = countWays(s, si, cut - 1, dp);
+                Pair righPair = countWays(s, cut + 1, ei, dp);
+
+                evaluateTrue(leftPair, righPair, res, s.charAt(cut));
+            }
+            return dp[si][ei] = res;
+        }
+
+        static int countWays(int N, String S) {
+            Pair[][] dp = new Pair[N][N];
+            return countWays(S, 0, N - 1, dp).TotalTrue;
+        }
+    }
+
+    // Optimal Binary Search Tree
+    // https://www.geeksforgeeks.org/optimal-binary-search-tree-dp-24/
+    private static int optimalBST(int keys[], int freq[], int si, int ei, int[][] dp) {
+        if (dp[si][ei] != 0) {
+            return dp[si][ei];
+        }
+        int minCost = (int) 1e8;
+        int sum = 0;
+        for (int idx = si; idx <= ei; idx++) {
+            sum += freq[idx];
+        }
+        for (int cut = si; cut <= ei; cut++) {
+            int leftCost = (cut == si) ? 0 : optimalBST(keys, freq, si, cut - 1, dp);
+            int rightCost = (cut == ei) ? 0 : optimalBST(keys, freq, cut + 1, ei, dp);
+            minCost = Math.min(minCost, leftCost + rightCost);
+        }
+        return dp[si][ei] = minCost + sum;
+    }
+
     public static void main(String[] args) {
         // String s = "1+2*3+4*5";
         // String s = "12*12";
-        String s = "11+2*342+4+11";
-        Pair[][] dp = new Pair[s.length()][s.length()];
-        Pair ans = MinAndMaxValueOfExp(s, 0, s.length() - 1, dp);
-        System.out.println(ans.min + " " + ans.max);
+        // String s = "11+2*342+4+11";
+        // Pair[][] dp = new Pair[s.length()][s.length()];
+        // Pair ans = MinAndMaxValueOfExp(s, 0, s.length() - 1, dp);
+        // System.out.println(ans.min + " " + ans.max);
+        int keys[] = { 10, 12, 20 };
+        int freq[] = { 34, 8, 50 };
+        int[][] dp = new int[keys.length][keys.length];
+        System.out.println(optimalBST(keys, freq, 0, keys.length - 1, dp));
     }
 }
